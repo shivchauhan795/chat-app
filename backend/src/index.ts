fc953100d268ea51d2df8d5667bd80007532ac24
Shiv Chauhan <shivchauhan795@gmail.com>
2025-02-23 16:26:51 +0530

    added more elaborative response to have better ux

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { WebSocketServer, WebSocket } from "ws";
-
+import { v4 as uuidv4 } from 'uuid';
 const wss = new WebSocketServer({ port: 8080 });
 
 interface User {
     socket: WebSocket;
     room: string;
+    name: string;
+    id: string;
 }
 let allSockets: User[] = [];
 
@@ -15,14 +17,25 @@ wss.on("connection", (socket) => {
         if (parsedMessage.type === "join") {
             allSockets.push({
                 socket,
-                room: parsedMessage.payload.roomId
+                room: parsedMessage.payload.roomId,
+                name: parsedMessage.payload.name,
+                id: uuidv4()
             })
         }
         if (parsedMessage.type === "chat") {
             const currentRoom = allSockets.find((x) => x.socket == socket)?.room;
+            const myId = allSockets.find((x) => x.socket == socket)?.id;
+            const myName = allSockets.find((x) => x.socket == socket)?.name;
 
             if (currentRoom) {
-                allSockets.filter((x) => x.room === currentRoom).forEach((x) => x.socket.send(parsedMessage.payload.message));
+                allSockets.filter((x) => x.room === currentRoom).forEach((x) => x.socket.send(JSON.stringify(
+                    {
+                        message: parsedMessage.payload.message,
+                        senderName: myName,
+                        senderId: myId
+                    }
+                )
+                ));
             }
 
         }
