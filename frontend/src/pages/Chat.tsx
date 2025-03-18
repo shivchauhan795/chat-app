54d7b230ee96f86f6ef90eb53c35557b1295355f
Shiv Chauhan <shivchauhan795@gmail.com>
2025-03-18 12:34:01 +0530

    emoji picker added

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -3,6 +3,9 @@ import { sendIcon } from "../icons/sendIcon.tsx";
 import { Input } from '../ui/Input.tsx';
 import { MessageBubble } from '../ui/MessageBubble.tsx';
 import { useParams } from 'react-router-dom';
+import EmojiPicker, { SkinTones } from 'emoji-picker-react';
+import { smileIcon } from '../icons/smileIcon.tsx';
+
 interface Data {
     message: string,
     senderName: string
@@ -17,6 +20,7 @@ function Chat() {
     const wsRef = useRef<WebSocket>(null);
     const inputRef = useRef<HTMLInputElement>(null);
     const messagesEndRef = useRef<HTMLDivElement>(null);  // keep the scroll at the bottom
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
     useEffect(() => {
         const ws = new WebSocket(backendURL);
@@ -46,9 +50,22 @@ function Chat() {
         }
     }, [messages]);
 
+    const handleEmojiClick = (emojiObject: { emoji: string; }) => {
+        if (inputRef.current) {
+            inputRef.current.value += emojiObject.emoji;
+        }
+    };
+
 
     return (
         <div className='flex flex-col justify-between w-full h-screen items-center bgimage text-white'>
+            <div className='fixed bottom-8 left-0'>
+                {showEmojiPicker && (
+                    <div className='fixed bottom-40 left-8 bg-white p-2 rounded-lg shadow-lg'>
+                        <EmojiPicker width={"auto"} onEmojiClick={handleEmojiClick} />
+                    </div>
+                )}
+            </div>
             <div className={`h-5/6 pt-16 flex flex-col gap-5 overflow-auto w-full justify-start items-end custom-scrollbar`}>
 
                 {data
@@ -66,11 +83,13 @@ function Chat() {
                     type="text"
                     placeholder="Message"
                     ref={inputRef}
+                    startIcon={smileIcon()}
                     endIcon={sendIcon()}
                     width='w-full'
                     onClick={
                         () => {
                             if (!inputRef.current) return;
+                            setShowEmojiPicker(false);
                             wsRef.current?.send(JSON.stringify({
                                 type: "chat",
                                 payload: {
@@ -81,6 +100,8 @@ function Chat() {
                             inputRef.current.value = "";
                         }
                     }
+                    onEmojiClick={() => setShowEmojiPicker(!showEmojiPicker)}
+                    onChange={() => setShowEmojiPicker(false)}
                 />
 
             </div>
--- frontend/src/ui/Input.tsx
+++ frontend/src/ui/Input.tsx
@@ -4,10 +4,13 @@ interface InputProps {
     type: string;
     placeholder: string;
     ref?: any
+    startIcon?: ReactElement;
     endIcon?: ReactElement;
     uppercase?: boolean
     width: string
     onClick?: () => void
+    onEmojiClick?: () => void
+    onChange?: () => void
 }
 
 const DefaultStyle = "px-3 py-1 h-3/5 border-2 rounded-2xl flex items-center justify-between outline-none";
@@ -15,12 +18,14 @@ const DefaultStyle = "px-3 py-1 h-3/5 border-2 rounded-2xl flex items-center jus
 export const Input = (props: InputProps) => {
     return (
         <div className={`${DefaultStyle} ${props.width}`} >
+            {props.startIcon && <span className="mr-2 cursor-pointer" onClick={props.onEmojiClick}>{props.startIcon}</span>}
             <input
                 className={`flex-1 p-1 outline-none selection:bg-yellow-900/30 ${props.uppercase && "uppercase"}`}
                 type={props.type}
                 placeholder={props.placeholder}
                 ref={props.ref}
                 onKeyDown={(e) => e.key === "Enter" && props.onClick?.()}   // on the click of enter the onClick function will be called
+                onChange={props.onChange} // detects typing and close the emohipicker
             />
             {props.endIcon && <span className="ml-2 cursor-pointer" onClick={props.onClick}>{props.endIcon}</span>}
         </div>
